9683daf8a3d673b099e3bafbeb93e44d3e3bc87b
punchholer <47150537+punchholer@users.noreply.github.com>
2023-12-08 20:49:17 +0800

    edit

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -1,11 +1,11 @@
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
        <div id="social-icons">
-            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="/img/icons/kofi-icon.png" alt="Ko-Fi"></a>
-            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="/img/icons/tapas-icon.png" alt="Tapastic"></a>
-            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="/img/icons/discord-icon.png" alt="Discord"></a>
-            <a href="https://twitter.com/efarcomic" target="_blank"><img src="/img/icons/twitter-icon.png" alt="Twitter"></a>
-            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw"><img src="/img/icons/youtube-icon.png" alt="Youtube"></a>
+            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="../img/icons/kofi-icon.png" alt="Ko-Fi"></a>
+            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="../img/icons/tapas-icon.png" alt="Tapastic"></a>
+            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="../img/icons/discord-icon.png" alt="Discord"></a>
+            <a href="https://twitter.com/efarcomic" target="_blank"><img src="../img/icons/twitter-icon.png" alt="Twitter"></a>
+            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw"><img src="../img/icons/youtube-icon.png" alt="Youtube"></a>
             <!-- Add more social media icons as needed -->
 </div>
         <p><strong>Powered by</strong></p>
