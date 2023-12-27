3de4cbecaed1572d616019cc50bdfbe5f661b1dd
punchholer <punchmix97@gmail.com>
2023-12-27 21:25:48 +0800

    icons

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -2,12 +2,12 @@ document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
         
        <div id="social-icons">
-            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="img/icons/kofi-icon.png" alt="Ko-Fi"></a>
-            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas-icon.png" alt="Tapastic"></a>
-            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord-icon.png" alt="Discord"></a>
-            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter-icon.png" alt="Twitter"></a>
-            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube-icon.png" alt="Youtube"></a>
-            <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss-icon.png" alt="RSS"></a>
+            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="img/icons/kofi.png" alt="Ko-Fi"></a>
+            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.png" alt="Tapastic"></a>
+            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.png" alt="Discord"></a>
+            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.png" alt="Twitter"></a>
+            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube.png" alt="Youtube"></a>
+            <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss-32.ico" alt="RSS"></a>
             <!-- Add more social media icons as needed -->   
     </div>
             <p>© 2017 - 2024 Punch-Holer | Powered by <a href="https://rarebit.neocities.org">Rarebit</a> | This website uses <a href="https://www.goatcounter.com/">GoatCounter</a></p>
