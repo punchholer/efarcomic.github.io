411f0a9c124cabe169bcbc6494d97df1dad2e369
punchholer <punchmix97@gmail.com>
2023-12-31 12:04:37 +0800

    css and icons fix

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -1,18 +1,17 @@
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
-        
-       <div id="social-icons">
-            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="img/icons/kofi.png" alt="Ko-Fi" height="16" style="width:75%;"></a>
-            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.png" alt="Tapastic" height="16" style="width:75%;"></a>
-            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.png" alt="Discord" height="16" style="width:75%;"></a>
-            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.png" alt="Twitter" height="16" style="width:75%;"></a>
-            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube.png" alt="Youtube" height="16" style="width:75%;"></a>
-            <a href="https://www.tumblr.com/efarcomic" target="_blank"><img src="img/icons/tumblr.png" alt="Tumblr" height="16" style="width:75%;"></a>
-            <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss-icon.png" alt="RSS" height="16" style="width:75%;"></a>
+        <div id="social-icons">
+            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="img/icons/kofi.png" alt="Ko-Fi" style="width: 32;"></a>
+            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.png" alt="Tapastic" style="width: 32;"></a>
+            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.png" alt="Discord" style="width: 32;"></a>
+            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.png" alt="Twitter" style="width: 32;"></a>
+            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube.png" alt="Youtube" style="width: 32;"></a>
+            <a href="https://www.tumblr.com/efarcomic" target="_blank"><img src="img/icons/tumblr.png" alt="Tumblr" style="width: 32;"></a>
+            <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss.png" alt="RSS" style="width: 32;"></a>
             <!-- Add more social media icons as needed -->   
-    </div>
-            <p>© 2017 - 2024 Punch-Holer | Powered by <a href="https://rarebit.neocities.org">Rarebit</a> | This website uses <a href="https://www.goatcounter.com/">GoatCounter</a></p>
-    </div>
+        </div>
+        <p>© 2017 - 2024 Punch-Holer | Powered by <a href="https://rarebit.neocities.org">Rarebit</a> | This website uses <a href="https://www.goatcounter.com/">GoatCounter</a></p>
     </footer>
 `;
-document.styleSheets[0].insertRule('.social-icons-container a { margin-right: 5px; }', 0);
+
+document.styleSheets[0].insertRule('.social-icons-container a { margin-right: 15px; }', 0);
