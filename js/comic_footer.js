9cf2aba53e38986d39f8f0cab745586e282dac4b
punchholer <punchmix97@gmail.com>
2024-01-05 00:51:22 +0800

    migration to cloudflare

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -1,10 +1,10 @@
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
         <div id="social-icons">
-            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="img/icons/kofi.png" alt="Ko-Fi" style="width: 32;"></a>
-            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.png" alt="Tapastic" style="width: 32;"></a>
-            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.png" alt="Discord" style="width: 32;"></a>
-            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.png" alt="Twitter" style="width: 32;"></a>
+            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src = "img/icons/kofi.svg" alt="kofi"></a>
+            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.svg" alt="Tapastic" style="width: 32;"></a>
+            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.svg" alt="Discord" style="width: 32;"></a>
+            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.svg" alt="Twitter" style="width: 32;"></a>
             <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube.png" alt="Youtube" style="width: 32;"></a>
             <a href="https://www.tumblr.com/efarcomic" target="_blank"><img src="img/icons/tumblr.png" alt="Tumblr" style="width: 32;"></a>
             <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss.png" alt="RSS" style="width: 32;"></a>
