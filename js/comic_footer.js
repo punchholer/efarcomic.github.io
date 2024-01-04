a569ebc6333281ed45176926e4b2fe4d472cf4c1
punchholer <punchmix97@gmail.com>
2024-01-05 01:02:18 +0800

    svg icons

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -1,13 +1,14 @@
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
         <div id="social-icons">
-            <a href="https://ko-fi.com/punchyholery" target="_blank"><img src = "img/icons/kofi.svg" alt="kofi"></a>
-            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.svg" alt="Tapastic" style="width: 32;"></a>
-            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.svg" alt="Discord" style="width: 32;"></a>
-            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.svg" alt="Twitter" style="width: 32;"></a>
-            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube.png" alt="Youtube" style="width: 32;"></a>
-            <a href="https://www.tumblr.com/efarcomic" target="_blank"><img src="img/icons/tumblr.png" alt="Tumblr" style="width: 32;"></a>
-            <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss.png" alt="RSS" style="width: 32;"></a>
+        <a href="https://ko-fi.com/punchyholery" target="_blank"><img src="img/icons/kofi.svg" alt="kofi" width="32" height="32"></a>
+
+            <a href="https://tapas.io/series/EFAR/info" target="_blank"><img src="img/icons/tapas.svg" alt="tapas" width="32" height="32"></a>
+            <a href="https://discord.gg/vnvMKxREHN" target="_blank"><img src="img/icons/discord.svg" alt="discord" width="32" height="32"></a>
+            <a href="https://twitter.com/efarcomic" target="_blank"><img src="img/icons/twitter.svg" alt="twitter" width="32" height="32"></a>
+            <a href="https://www.youtube.com/channel/UCq02sauux-1yqpIly8wI7Nw" target="_blank"><img src="img/icons/youtube.svg" alt="youtube" width="32" height="32"></a>
+            <a href="https://www.tumblr.com/efarcomic" target="_blank"><img src="img/icons/tumblr.svg" alt="tumblr" width="32" height="32"></a>
+            <a href="https://efarcomic.com/rss.xml" target="_blank"><img src="img/icons/rss.svg" alt="rss" width="32" height="32"></a>
             <!-- Add more social media icons as needed -->   
         </div>
         <p>© 2017 - 2024 Punch-Holer | Powered by <a href="https://rarebit.neocities.org">Rarebit</a> | This website uses <a href="https://www.goatcounter.com/">GoatCounter</a></p>
